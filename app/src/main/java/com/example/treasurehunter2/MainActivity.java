65fe01cb5a48bb3b8d463eacb0589c19115ab9ed
Entruna <im.tired94@hotmail.com>
2021-07-13 12:25:00 +0200

    blank lines delete

--- app/src/main/java/com/example/treasurehunter2/MainActivity.java
+++ app/src/main/java/com/example/treasurehunter2/MainActivity.java
@@ -1,15 +1,12 @@
 package com.example.treasurehunter2;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
-
 public class MainActivity extends AppCompatActivity {
     private static int TIME_OUT = 6000;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,11 +18,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(homeIntent);
                 finish();
             }
-        },TIME_OUT);
-
-
-
+        }, TIME_OUT);
     }
-
-
 }
